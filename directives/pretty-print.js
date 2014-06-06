57fac667cde16a3e9261ead392771785562ce153
Aaron Roberson <aaronaroberson@gmail.com>
2014-06-06 10:35:40 -0700

    Added pretty print directive

---
+++ directives/pretty-print.js
@@ -0,0 +1,8 @@
+App.directive('prettyprint', function() {
+	return {
+		restrict: 'C',
+		link: function postLink(scope, element, attrs) {
+			element.html(prettyPrintOne(element.html()));
+		}
+	};
+});
