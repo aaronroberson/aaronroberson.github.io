92bf4ffdd49a96c98ec3b32accccbd8cd8b496e9
Aaron Roberson <aaronaroberson@gmail.com>
2014-06-06 10:44:44 -0700

    Removed comments

--- directives/pretty-print.js
+++ directives/pretty-print.js
@@ -2,7 +2,6 @@ angular.module('aaronroberson').directive('prettyprint', function() {
 	return {
 		restrict: 'C',
 		link: function postLink(scope, element, attrs) {
-			console.log(element.html());
 			element.html(prettyPrintOne(element.html()));
 		}
 	};
