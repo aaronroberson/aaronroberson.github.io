2699fa367eccf752b377e67581c3039452624dff
Aaron Roberson <aaronaroberson@gmail.com>
2014-06-06 10:44:04 -0700

    More prettify stuff

--- directives/pretty-print.js
+++ directives/pretty-print.js
@@ -2,6 +2,7 @@ angular.module('aaronroberson').directive('prettyprint', function() {
 	return {
 		restrict: 'C',
 		link: function postLink(scope, element, attrs) {
+			console.log(element.html());
 			element.html(prettyPrintOne(element.html()));
 		}
 	};
